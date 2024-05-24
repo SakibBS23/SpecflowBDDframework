cc1825d3e0c46e86e525945ae05c77cf92900208
Nazmus Sakib <sakib.nazmus@brainstation-23.com>
2024-05-24 20:23:27 +0600

    24.5.2024update

--- TealiumSpecFlowProject/StepDefinitions/FeatureStepDefinitions.cs
+++ TealiumSpecFlowProject/StepDefinitions/FeatureStepDefinitions.cs
@@ -1,6 +1,5 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Interactions;
 
 namespace TealiumSpecFlowProject.StepDefinitions
 {
@@ -8,35 +7,40 @@ namespace TealiumSpecFlowProject.StepDefinitions
     public sealed class FeatureStepDefinitions
     {
         private IWebDriver driver;
+        public FeatureStepDefinitions(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
         [Given(@"Open the browser")]
         public void GivenOpenTheBrowser()
         {
-            driver= new ChromeDriver();
+            /*
+            driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(50);
             driver.Manage().Window.Maximize();
+            */
         }
 
         [When(@"Enter the URL")]
         public void WhenEnterTheURL()
         {
-            driver.Url = "https://ecommerce.tealiumdemo.com/customer/account/login/";
+            driver.Url = "https://ecommerce.tealiumdemo.com/";
 
         }
 
         [Then(@"Login for the Tealium")]
         public void ThenLoginForTheTealium()
         {
+            driver.FindElement(By.XPath("(//span[@class='label'])[3]")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(),'Log In')]")).Click();
+            Thread.Sleep(5000);
             driver.FindElement(By.XPath("(//input[contains(@class,'input-text required-entry')])[2]")).SendKeys("n@gmail.com");
             driver.FindElement(By.XPath("(//input[@class='input-text required-entry'])[2]")).SendKeys("1234567");
             Thread.Sleep(3000);
-            var element = driver.FindElement(By.Id("send2"));
-            Actions actions = new Actions(driver);
-            actions.MoveToElement(element);
-            actions.Perform();
-            element.Submit();
-            
+            driver.FindElement(By.XPath("//span[text()='Login']")).Submit();
             Thread.Sleep(3000);
-            driver.Close();
+            //driver.Close();
+
         }
     }
 }
